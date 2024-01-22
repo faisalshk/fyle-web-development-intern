6ad5ccb4e469d6397a43c2eeea1801c46aa68e2f
faisalshk <86406064+faisalshk@users.noreply.github.com>
2024-01-22 16:34:09 +0530

    loader bug solved

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 "use strict";
+
+// DOM elements
 const input = document.querySelector(".input");
 const form = document.querySelector(".form");
 const profile = document.querySelector(".profile");
@@ -11,33 +13,27 @@ const spinnerUser = document.querySelector(".spinnerUser");
 const spinnerRepo = document.querySelector(".spinnerRepo");
 const parent = document.querySelector(".parent");
 
-let pageButtons;
+// State variables
 let userName = "";
 let pageNumber = 1;
 let totalPages = 0;
-//////////////////////////////////////////////////////////////////////////////
+let pageButtons;
+
+// Fetch user data from GitHub API
 const getUser = async function (userName) {
   try {
-    const fetchData = fetch(`https://api.github.com/users/${userName}`);
-    const res = await fetchData;
+    const res = await fetch(`https://api.github.com/users/${userName}`);
     const userData = await res.json();
-
-    // Log the data to the console
-    console.log(userData);
 
     if (res.ok && userData) {
       spinnerUser.style.display = "none";
-      // Update the profile data in the DOM
       updateProfile(userData);
-
       profile.style.opacity = 1;
 
       totalPages =
         Math.ceil(userData.public_repos / 10) > 10
           ? 10
           : Math.ceil(userData.public_repos / 10);
-
-      console.log(totalPages);
 
       if (totalPages > 1) {
         paginationButtons(totalPages);
@@ -55,24 +51,17 @@ const getUser = async function (userName) {
   }
 };
 
-///////////////////////////////////////////////////////////////////////////////
+// Fetch user repositories from GitHub API
 const getUserRepo = async function (userName, pageNumber = 1) {
-  console.log(totalPages);
   try {
     spinnerRepo.style.display = "flex";
-    const fetchRepoData = fetch(
+    const res = await fetch(
       `https://api.github.com/users/${userName}/repos?page=${pageNumber}&per_page=10`
     );
-
-    const res = await fetchRepoData;
-
     const repoData = await res.json();
-
-    console.log(repoData);
 
     if (res.ok && repoData && repoData.length > 0) {
       spinnerRepo.style.display = "none";
-
       updateRepository(repoData);
 
       if (totalPages > 1) updateActivePagination(pageNumber);
@@ -80,7 +69,6 @@ const getUserRepo = async function (userName, pageNumber = 1) {
       repository.style.opacity = 1;
 
       const errorRepoDiv = document.querySelector(".error-repo");
-
       errorRepoDiv.style.display = "none";
     } else {
       throw new Error("No Repository Found");
@@ -91,88 +79,81 @@ const getUserRepo = async function (userName, pageNumber = 1) {
   }
 };
 
-//////////////////////////////////////////////////////////////////////////
+// Display user-related error messages
 const displayError = function (error) {
-  let el = document.querySelector(".error-user");
-  el.innerHTML = error;
-  el.style.display = "block";
-
-  // hide the profile
+  displayErrorElement(".error-user", error);
   profile.style.opacity = 0;
   paginationContainer.style.opacity = 0;
+  spinnerUser.style.display = "none";
 };
 
-///////////////////////////////////////////////////////////////////////////
+// Display repository-related error messages
 const displayErrorRepo = function (error) {
-  let el = document.querySelector(".error-repo");
+  displayErrorElement(".error-repo", error);
+  repository.style.opacity = 0;
+  spinnerRepo.style.display = "none";
+};
+
+// Display error message in the specified element
+const displayErrorElement = function (elementSelector, error) {
+  let el = document.querySelector(elementSelector);
   el.innerHTML = error;
   el.style.display = "block";
-
-  // hide the profile
-  repository.style.opacity = 0;
-  // paginationContainer.style.opacity = 0;
 };
 
-// Function to update the profile data in the DOM
+// Update profile data in the DOM
 const updateProfile = function (userData) {
   profile.innerHTML = "";
   let html = `
-  <div class="profile-pic">
-          <img src="${userData.avatar_url}" alt="Profile Pic" />
-        </div>
-        <div class="profile-info">
-          <h1>${userData.name}</h1>
-          <p>${userData.bio}</p>
-          <h4><span>Location: </span>${userData.location}</h4>
-        </div>
+    <div class="profile-pic">
+      <img src="${userData.avatar_url}" alt="Profile Pic" />
+    </div>
+    <div class="profile-info">
+      <h1>${userData.name}</h1>
+      <p>${userData.bio}</p>
+      <h4><span>Location: </span>${userData.location}</h4>
+    </div>
   `;
   profile.insertAdjacentHTML("afterbegin", html);
-
-  // profile.style.opacity = 100;
 };
 
-// function to update repository data in the dom
+// Update repository data in the DOM
 const updateRepository = function (repoData) {
   const repoContainer = document.querySelector(".repository");
-
   repoContainer.innerHTML = "";
 
   repoData.forEach((data) => {
     const html = `
-    <a class="card" href=${data.html_url} target=_blank>
-    <h2>${data.name}</h2>
-    <p>${data.description || "No description available"}</p>
-    <div class="topic">
-    ${
-      data.topics.length > 0
-        ? data.topics.map((topic) => `<span>${topic}</span>`).join("")
-        : "no topics available"
-    }
-    </div>
-  </a>
-
+      <a class="card" href=${data.html_url} target=_blank>
+        <h2>${data.name}</h2>
+        <p>${data.description || "No description available"}</p>
+        <div class="topic">
+          ${
+            data.topics.length > 0
+              ? data.topics.map((topic) => `<span>${topic}</span>`).join("")
+              : "no topics available"
+          }
+        </div>
+      </a>
     `;
     repoContainer.insertAdjacentHTML("afterbegin", html);
   });
 };
 
-// pagination function////////////////////////////////////////////
+// Create pagination buttons
 const paginationButtons = function (totalPages) {
   pageContainer.innerHTML = "";
   for (let i = 1; i <= totalPages; i++) {
     const pageBtn = document.createElement("h4");
     pageBtn.classList.add("pg");
     pageBtn.textContent = i;
-    pageBtn.addEventListener("click", (e) => {
-      pageHandler(e);
-    });
+    pageBtn.addEventListener("click", pageHandler);
     pageContainer.appendChild(pageBtn);
   }
   pageButtons = document.querySelectorAll(".pg");
-  console.log(pageButtons);
 };
 
-// add active class to the pagination buttons////////////////////
+// Add active class to the pagination buttons
 const updateActivePagination = function (activePage) {
   pageButtons.forEach((btn) => {
     btn.classList.remove("active");
@@ -182,7 +163,7 @@ const updateActivePagination = function (activePage) {
   });
 };
 
-///////////////////////////////////////////////////////////////////////
+// Handle form submission
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
   userName = input.value;
@@ -205,34 +186,12 @@ form.addEventListener("submit", async (e) => {
   }
 });
 
-//////////////////////////////////////////////////////////////////////
-// next and prev buttons
-
-prevBtn.addEventListener("click", () => {
-  pageNumber--;
-  repository.style.opacity = 0;
-  if (pageNumber === 1) {
-    prevBtn.disabled = true;
-  }
-  nextBtn.disabled = false;
-  getUserRepo(userName, pageNumber);
-});
-
-nextBtn.addEventListener("click", function () {
-  pageNumber++;
-  repository.style.opacity = 0;
-  if (pageNumber === totalPages) {
-    nextBtn.disabled = true;
-  }
-  prevBtn.disabled = false;
-  getUserRepo(userName, pageNumber);
-});
-
+// Handle pagination button click
 const pageHandler = function (e) {
   const page = Number(e.target.innerHTML);
   pageNumber = page;
-  console.log(page);
   repository.style.opacity = 0;
+
   if (pageNumber === 1) {
     prevBtn.disabled = true;
     nextBtn.disabled = false;
@@ -243,5 +202,28 @@ const pageHandler = function (e) {
     prevBtn.disabled = false;
     nextBtn.disabled = false;
   }
+
   getUserRepo(userName, page);
 };
+
+// Handle previous button click
+prevBtn.addEventListener("click", () => {
+  pageNumber--;
+  repository.style.opacity = 0;
+  if (pageNumber === 1) {
+    prevBtn.disabled = true;
+  }
+  nextBtn.disabled = false;
+  getUserRepo(userName, pageNumber);
+});
+
+// Handle next button click
+nextBtn.addEventListener("click", function () {
+  pageNumber++;
+  repository.style.opacity = 0;
+  if (pageNumber === totalPages) {
+    nextBtn.disabled = true;
+  }
+  prevBtn.disabled = false;
+  getUserRepo(userName, pageNumber);
+});
